09e95dbc426529d605ae8753da1244ecefb79a01
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-13 02:28:28 +0530

    Centralize static styles

--- main/renderer.py
+++ main/renderer.py
@@ -1,6 +1,7 @@
-from .env import PUBNAME
+from .env import PUBNAME, MAILUSER
 from django.shortcuts import render
 
 def renderView(request, view, data={}):
     data['appname'] = PUBNAME
+    data['contactmail'] = MAILUSER
     return render(request, view, data)
--- service/admin.py
+++ service/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 from .models import Service
 # Register your models here.
 admin.site.register(Service)
+admin.site.register(Session)
