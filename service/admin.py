d1409bd7da85ef8c8a83ce99f9cf053aebced2d9
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-13 20:29:07 +0530

    Create admin view filters

--- service/admin.py
+++ service/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from django.contrib.sessions.models import Session
 from .models import Service
 # Register your models here.
-admin.site.register(Service)
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["name", "url"]
+    def get_queryset(self,request):
+        query_set = super(ServiceAdmin,self).get_queryset(request)
+        return query_set
+    class Meta:
+        ordering = ("")
+
+
 admin.site.register(Session)
