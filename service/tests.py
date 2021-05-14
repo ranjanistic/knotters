27f43680d6897f0b01110591a33ed5bbae0e66d3
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-14 19:10:48 +0530

    Create proper workflows

--- service/tests.py
+++ service/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+# class ServiceTestCase(TestCase):
+#     def setUp(self):
+#         Service.objects.create(name="TestService", url="testurl.myapp.xyz", description="Nothing new")
+
+#     def test_service(self):
+#         testservice = Service.objects.get(name="TestService")
+#         self.assertEqual(testservice.url, "testurl.myapp.xyz")
