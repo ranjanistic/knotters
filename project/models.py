7f0889638d5362618b4d55a2ad6c7413919a18e9
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-19 01:19:15 +0530

    Project & org repo creation works

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 def projectImagePath(instance,filename):
     return 'projects/{}/'.format(str(instance.id))+'/{}'.format(filename)
@@ -15,11 +16,12 @@ class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50,null=False,blank=False)
     url = models.CharField(max_length=500,null=True,blank=True)
+    creator = models.UUIDField(editable=False, null=False, blank=False)
     image = models.FileField(upload_to=projectImagePath,null=True,blank=True,max_length=500)
-    reponame = models.URLField(max_length=500,null=False,blank=False)
-    githuburl = models.URLField(max_length=500,null=True,blank=True)
+    reponame = models.CharField(max_length=500,null=False,blank=False,unique=True)
     description = models.CharField(max_length=5000,null=False,blank=False)
     tags = models.ManyToManyField(Tag)
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     def __str__(self):
         return self.name
 
--- project/views.py
+++ project/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from main.renderer import renderView
-from .models import Project
+from .models import Project, Tag
+from main.env import GITHUBBOTTOKEN, PUBNAME
+from github import Github
 
 def allProjects(request):
     projects = Project.objects.all()
@@ -13,43 +16,100 @@ def profile(request,id):
 
 @login_required
 def create(request):
+    # user = User.objects.get()
     data = {'step':1, 'totalsteps':3 }
-    print(request.POST)
     try:
-        # step 3
-        projectabout = request.POST['projectabout']
-        tags = request.POST['tags']
-        projectname = request.POST['projectname']
-        reponame = request.POST['reponame']
+        # step 3 submit
+        projectabout = str(request.POST['projectabout']).strip()
+        tags = str(request.POST['tags']).strip().split(',')
+        projectname = str(request.POST['projectname']).strip()
+        reponame = str(request.POST['reponame']).strip()
         nextstep = int(request.POST['nextstep'])
+        data["projectabout"] = projectabout
+        data["tags"] = str(request.POST['tags']).strip()
+        data["projectname"] = projectname
+        data["reponame"] = reponame
         if nextstep != 4:
             raise Exception()
-        else: return redirect('/') # create project
+        else:
+            project = createProject(projectname,reponame,projectabout,tags,None)
+            if not project:
+                data['step'] = 3
+                data['errorfinal'] = 'An error occurred while creating your project.'
+                return renderView(request,'project/create.html', data)
+            else:
+                return redirect(f"/projects/profile/{project.id}")
     except:
         try:
-            # step 2
-            projectabout = request.POST['projectabout']
-            tags = request.POST['tags']
-            projectname = request.POST['projectname']
-            reponame = request.POST['reponame']
-            data['step'] = int(request.POST['nextstep'])
-            data["projectname"] = projectname
-            data["reponame"] = reponame
+            # step 2 submit
+            projectabout = str(request.POST['projectabout']).strip()
+            tags = str(request.POST['tags']).strip()
             data["projectabout"] = projectabout
             data["tags"] = tags
+            projectname = str(request.POST['projectname']).strip()
+            reponame = str(request.POST['reponame']).strip()
+            data["projectname"] = projectname
+            data["reponame"] = reponame
+            data['step'] = 2
+            if projectabout == "":
+                data['errorprojectabout'] = "Please write something about what this is going to be."
+            elif tags == "":
+                data['errortags'] = "Assign atleast one keyword relevant to your project."
+            else:
+                data['step'] = int(request.POST['nextstep'])
             return renderView(request,'project/create.html', data)
         except:
             try:
-                # step 1
-                projectname = request.POST['projectname']
-                reponame = request.POST['reponame']
-                data['step'] = int(request.POST['nextstep'])
+                # step 1 submit
+                projectname = str(request.POST['projectname']).strip()
+                reponame = str(request.POST['reponame']).strip()
                 data["projectname"] = projectname
                 data["reponame"] = reponame
-                projectabout = request.POST['projectabout']
-                tags = request.POST['tags']
-                data["projectabout"] = projectabout
-                data["tags"] = tags
+                if projectname == "":
+                    data['errorprojectname'] = "Project display name is required"
+                elif not uniqueRepoName(reponame):
+                    data['errorreponame'] = f"{reponame} already exists"
+                else:
+                    data['step'] = int(request.POST['nextstep'])
+                    projectabout = request.POST['projectabout']
+                    tags = request.POST['tags']
+                    data["projectabout"] = projectabout
+                    data["tags"] = tags
             except:
                 pass
         return renderView(request,'project/create.html', data)
+
+
+def uniqueRepoName(reponame):
+    try:
+        Project.objects.get(reponame=reponame)
+    except:
+        return True
+    return False
+
+def uniqueTag(tagname):
+    try:
+        Tag.objects.get(name=tagname)
+    except:
+        return True
+    return False
+
+
+# Creates project on knotters & then repository in github/Knotters using knottersbot
+def createProject(name,reponame,description,tags,user):
+    return False
+    try:
+        project = Project.objects.create(creator=user,name=name,reponame=reponame,description=description)
+        for tag in tags:
+            if uniqueTag(tag):
+                tagobj = Tag.objects.create(name=tag)
+            else:
+                tagobj = Tag.objects.get(name=tag)
+            project.tags.add(tagobj)
+
+        g = Github(GITHUBBOTTOKEN)
+        org = g.get_organization(PUBNAME)
+        repo = org.create_repo(name,private=False,description=description, auto_init=True)
+        return project
+    except:
+        return False
