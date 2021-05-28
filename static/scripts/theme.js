383cd1754b86d6b0141ed0c4d266b18e6df0ac33
ranjanistic <priyanshuranjan88@gmail.com>
2021-05-29 01:42:33 +0530

    Add image change as per theme support

--- static/scripts/theme.js
+++ static/scripts/theme.js
@@ -1,50 +1,57 @@
 const theme = {
-  key: "theme",
-  light: "light",
-  dark: "dark",
+    key: "theme",
+    light: "light",
+    dark: "dark",
 };
 
 Array.from(document.getElementsByClassName("themeswitch")).forEach((elem) => {
-  elem.onclick = (_) => toggleTheme();
+    elem.onclick = (_) => toggleTheme();
 });
 
 const toggleTheme = () => {
-  isLight() ? setDark() : setLight();
+    isLight() ? setDark() : setLight();
 };
 const setDark = () => {
-  setTheme(theme.dark);
+    setTheme(theme.dark);
 };
 const setLight = () => {
-  setTheme(theme.light);
+    setTheme(theme.light);
 };
 const isLight = () => {
-  return getTheme() == theme.light;
+    return getTheme() == theme.light;
 };
 const isDark = () => {
-  return getTheme() == theme.dark;
+    return getTheme() == theme.dark;
 };
 
 const getTheme = () => {
-  return localStorage.getItem(theme.key);
+    return localStorage.getItem(theme.key);
 };
 
 const primarycolor = {
-  [theme.dark]: "#000000",
-  [theme.light]: "#ffffff",
+    [theme.dark]: "#000000",
+    [theme.light]: "#ffffff",
 };
 
 const setTheme = (themevalue = theme.light) => {
-  localStorage.setItem(theme.key, themevalue);
-  window.parent.document
-    .getElementById("themecolor")
-    .setAttribute("content", primarycolor[themevalue]);
-  document.documentElement.setAttribute("data-theme", themevalue);
-  window.parent.document.documentElement.setAttribute("data-theme", themevalue);
+    localStorage.setItem(theme.key, themevalue);
+    window.parent.document
+        .getElementById("themecolor")
+        .setAttribute("content", primarycolor[themevalue]);
+    document.documentElement.setAttribute("data-theme", themevalue);
+    window.parent.document.documentElement.setAttribute(
+        "data-theme",
+        themevalue
+    );
+    Array.from(document.getElementsByClassName("darkimg")).forEach((img) => {
+        img.src = `/static/graphics/${img.getAttribute("data-name")}${
+            themevalue === theme.light ? "" : "-dark"
+        }.${img.getAttribute("data-ext")}`;
+    });
 };
 
-localStorage.getItem(theme.key)
-  ? document.documentElement.setAttribute(
-      "data-theme",
-      localStorage.getItem(theme.key)
-    )
-  : localStorage.setItem(theme.key, theme.light);
+if (!localStorage.getItem(theme.key)) {
+    localStorage.setItem(theme.key, theme.light);
+}
+
+setTheme(localStorage.getItem(theme.key));
