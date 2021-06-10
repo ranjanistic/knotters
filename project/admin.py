ba447ba0df2b150ee8cf1b95d7de963d4053e61d
ranjanistic <priyanshuranjan88@gmail.com>
2021-06-10 23:34:27 +0530

    Add category model + discord module

--- project/admin.py
+++ project/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from django.contrib.sessions.models import Session
-from .models import Project, Tag
+from .models import Category, Project, Tag
 
+admin.site.register(Tag)
 
-# Register your models here.
+admin.site.register(Category)
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ["name", "url"]
+    list_display = ["name", "reponame", "status"]
+    list_filter = ["status", "category", "creator"]
     def get_queryset(self,request):
         query_set = super(ProjectAdmin,self).get_queryset(request)
         return query_set
     class Meta:
         ordering = ("")
-
-
-admin.site.register(Tag)
 
 admin.site.register(Session)
