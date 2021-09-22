7f026210103e5d199663a1ff9d8ae1ef0b1ca46a
ranjanistic <priyanshuranjan88@gmail.com>
2021-09-23 02:08:19 +0530

    Translation init

--- main/settings.py
+++ main/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from .strings import url, DIVISIONS, PEOPLE, AUTH
 from . import env
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = os.path.join(BASE_DIR, "apps")
@@ -74,7 +75,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "main.urls"
 
-BYPASS_2FA_PATHS = (url.ROBOTS_TXT,url.MANIFEST,url.SERVICE_WORKER,url.SWITCH_LANG, url.VERIFY_CAPTCHA)
+BYPASS_2FA_PATHS = (url.ROBOTS_TXT, url.MANIFEST,
+                    url.SERVICE_WORKER, url.SWITCH_LANG, url.VERIFY_CAPTCHA)
 
 TEMPLATES = [
     {
@@ -169,7 +171,7 @@ CACHE_SHORT = CACHE_MIN * 3
 CACHE_LONG = CACHE_SHORT * 2
 CACHE_LONGER = CACHE_LONG * 2
 CACHE_MAX = CACHE_LONGER * 2
-CACHE_ETERNAL= CACHE_MAX * 12 * 6
+CACHE_ETERNAL = CACHE_MAX * 12 * 6
 
 CACHE_MINI = CACHE_MIN
 
@@ -257,10 +259,8 @@ DEFAULT_CHARSET = 'utf-8'
 LANGUAGE_CODE = "en"
 LANGUAGE_COOKIE_NAME = "client_language"
 LANGUAGE_COOKIE_SECURE = True
-# LANGUAGES = (
-#     ('af', 'Afrikaans')('ar', 'Arabic')('ast', 'Asturian')('az', 'Azerbaijani')('bg', 'Bulgarian')('be', 'Belarusian')('bn', 'Bengali')('br', 'Breton')('bs', 'Bosnian')('ca', 'Catalan')('cs', 'Czech')('cy', 'Welsh')('da', 'Danish')('de', 'German')('dsb', 'Lower Sorbian')('el', 'Greek')('en', 'English')('en-au', 'Australian English')('en-gb', 'British English')('eo', 'Esperanto')('es', 'Spanish')('es-ar', 'Argentinian Spanish')('es-co', 'Colombian Spanish')('es-mx', 'Mexican Spanish')('es-ni', 'Nicaraguan Spanish')('es-ve', 'Venezuelan Spanish')('et', 'Estonian')('eu', 'Basque')('fa', 'Persian')('fi', 'Finnish')('fr', 'French')('fy', 'Frisian')('ga', 'Irish')('gd', 'Scottish Gaelic')('gl', 'Galician')('he', 'Hebrew')('hi', 'Hindi')('hr', 'Croatian')('hsb', 'Upper Sorbian')('hu', 'Hungarian')('hy', 'Armenian')('ia', 'Interlingua')('id', 'Indonesian')('io', 'Ido')
-#     ('is','Icelandic')('it', 'Italian')('ja', 'Japanese')('ka', 'Georgian')('kab', 'Kabyle')('kk', 'Kazakh')('km', 'Khmer')('kn', 'Kannada')('ko', 'Korean')('lb', 'Luxembourgish')('lt', 'Lithuanian')('lv', 'Latvian')('mk', 'Macedonian')('ml', 'Malayalam')('mn', 'Mongolian')('mr', 'Marathi')('my', 'Burmese')('nb', 'Norwegian Bokmål')('ne', 'Nepali')('nl', 'Dutch')('nn', 'Norwegian Nynorsk')('os', 'Ossetic')('pa', 'Punjabi')('pl', 'Polish')('pt', 'Portuguese')('pt-br', 'Brazilian Portuguese')('ro', 'Romanian')('ru', 'Russian')('sk', 'Slovak')('sl', 'Slovenian')('sq', 'Albanian')('sr', 'Serbian')('sr-latn', 'Serbian Latin')('sv', 'Swedish')('sw', 'Swahili')('ta', 'Tamil')('te', 'Telugu')('th', 'Thai')('tr', 'Turkish')('tt', 'Tatar')('udm', 'Udmurt')('uk', 'Ukrainian')('ur', 'Urdu')('uz', 'Uzbek')('vi', 'Vietnamese')('zh-hans', 'Simplified Chinese')('zh-hant', 'Traditional Chinese')
-# )
+LANGUAGES = (('af', _('Afrikaans')), ('ar', _('Arabic')), ('ast', _('Asturian')), ('az', _('Azerbaijani')), ('bg', _('Bulgarian')), ('be', _('Belarusian')), ('bn', _('Bengali')), ('br', _('Breton')), ('bs', _('Bosnian')), ('ca', _('Catalan')), ('cs', _('Czech')), ('cy', _('Welsh')), ('da', _('Danish')), ('de', _('German')), ('dsb', _('Lower Sorbian')), ('el', _('Greek')), ('en', _('English')), ('en-au', _('Australian English')), ('en-gb', _('British English')), ('eo', _('Esperanto')), ('es', _('Spanish')), ('es-ar', _('Argentinian Spanish')), ('es-co', _('Colombian Spanish')), ('es-mx', _('Mexican Spanish')), ('es-ni', _('Nicaraguan Spanish')), ('es-ve', _('Venezuelan Spanish')), ('et', _('Estonian')), ('eu', _('Basque')), ('fa', _('Persian')), ('fi', _('Finnish')), ('fr', _('French')), ('fy', _('Frisian')), ('ga', _('Irish')), ('gd', _('Scottish Gaelic')), ('gl', _('Galician')), ('he', _('Hebrew')), ('hi', _('Hindi')), ('hr', _('Croatian')), ('hsb', _('Upper Sorbian')), ('hu', _('Hungarian')), ('hy', _('Armenian')), ('ia', _('Interlingua')), ('id', _('Indonesian')), ('io', _('Ido')), ('is', _('Icelandic')), ('it', _('Italian')), ('ja', _('Japanese')), ('ka', _('Georgian')), ('kab', _('Kabyle')), ('kk', _('Kazakh')), ('km', _('Khmer')), ('kn', _('Kannada')), ('ko', _('Korean')), ('lb', _('Luxembourgish')), ('lt', _('Lithuanian')), ('lv', _('Latvian')), ('mk', _('Macedonian')), ('ml', _('Malayalam')), ('mn', _('Mongolian')), ('mr', _('Marathi')), ('my', _('Burmese')), ('nb', _('Norwegian Bokmål')), ('ne', _('Nepali')), ('nl', _('Dutch')), ('nn', _('Norwegian Nynorsk')), ('os', _('Ossetic')), ('pa', _('Punjabi')), ('pl', _('Polish')), ('pt', _('Portuguese')), ('pt-br', _('Brazilian Portuguese')), ('ro', _('Romanian')), ('ru', _('Russian')), ('sk', _('Slovak')), ('sl', _('Slovenian')), ('sq', _('Albanian')), ('sr', _('Serbian')), ('sr-latn', _('Serbian Latin')), ('sv', _('Swedish')), ('sw', _('Swahili')), ('ta', _('Tamil')), ('te', _('Telugu')), ('th', _('Thai')), ('tr', _('Turkish')), ('tt', _('Tatar')), ('udm', _('Udmurt')), ('uk', _('Ukrainian')), ('ur', _('Urdu')), ('uz', _('Uzbek')), ('vi', _('Vietnamese')), ('zh-hans', _('Simplified Chinese')), ('zh-hant', _('Traditional Chinese')))
+
 TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_L10N = True
