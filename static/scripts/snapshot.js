900944dd50ffc0d48abac2bd511fbfbec8d0da7f
Priyanshu Ranjan <priyanshuranjan88@gmail.com>
2021-12-07 02:14:22 +0530

    Revert "Revert "Update snapshots js""

--- static/scripts/snapshot.js
+++ static/scripts/snapshot.js
@@ -1,33 +1,33 @@
-let viewedSnaps = []
-
-const viewers = getElements("snapshot-viewer");
-
-const loadBrowseSnaps = async () => {
-    let viewer = viewers.find(view=>view.innerHTML.trim()=="")
+const loadBrowseSnaps = async (excludeIDs=[]) => {
+    const viewers = getElements("snapshot-viewer");
+    let viewer = viewers.find(view=>(view.innerHTML.trim()==""))
     if(!viewer){
        viewer = viewers[viewers.length - 1]
     }
     let snapdata = await postRequest(setUrlParams(URLS.BROWSER,"project-snapshots"), {
-       excludeIDs: viewedSnaps
+       excludeIDs
     });
     if(!snapdata) return false;
     if(snapdata.code === code.OK && snapdata.snapIDs.length) {
-       viewedSnaps = viewedSnaps.concat(snapdata.snapIDs)
        viewer.innerHTML += snapdata.html
-       return true
+       return excludeIDs.concat(snapdata.snapIDs)
     }
    return false
 }
 
-if(viewers.length){
-    (async()=>{
+const loadSnapshotScroller = async () => {
+    const viewers = getElements("snapshot-viewer");
+    if(viewers.length){
+        let viewedSnaps = []
         let done = await loadBrowseSnaps();
         if(done) {
+            viewedSnaps = viewedSnaps.concat(done)
             window.addEventListener("scroll", async ()=>{
-                if(done && document.body.scrollTop+document.body.offsetHeight+100 > viewers[0].offsetHeight){              
-                    done = await loadBrowseSnaps();
-                })
-            }
+                if(done && (document.body.scrollTop+document.body.offsetHeight+100) > viewers[0].offsetHeight){
+                    viewedSnaps = viewedSnaps.concat(done);    
+                    done = await loadBrowseSnaps(viewedSnaps);
+                }
+            });
         }
-    })();
+    }
 }
