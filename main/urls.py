81cb5d22599c8a991b33a7bdf71a9a0dd29122b4
Priyanshu Ranjan <priyanshuranjan88@gmail.com>
2021-12-11 15:35:18 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -50,6 +50,7 @@ urlpatterns = [
     path(URL.FAME_WALL, fameWall),
     path(URL.BROWSER, browser),
     path(URL.BASE_GITHUB_EVENTS, githubEventsListener),
+    path(URL.SNAPSHOT, snapshot),
     path('email/<str:template>', mailtemplate),
     path('template/<str:template>', template),
     path(ADMINPATH, admin.site.urls),
