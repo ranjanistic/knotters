d8bb64fc6c9744b6028702a1a73ae4b3c1c2e6d4
ranjanistic <priyanshuranjan88@gmail.com>
2022-01-02 02:11:19 +0530

    Add country str name

--- people/models.py
+++ people/models.py
@@ -121,11 +121,17 @@ class Country(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
     
 class State(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='state_country')
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class PhoneNumber(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
