7214c6bed1e29ca5e036aba29ecccba6b737d34b
ranjanistic <priyanshuranjan88@gmail.com>
2022-01-10 23:05:33 +0530

    auth/ login required

--- auth2/views.py
+++ auth2/views.py
@@ -1,6 +1,8 @@
 from main.strings import Template
 from .methods import renderer
+from main.decorators import normal_profile_required
 
+@normal_profile_required
 def auth_index(request):
     return renderer(request, Template.Auth.INDEX)
     
