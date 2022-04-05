43f6d991611986a136bd8b928ba9dddd8158d9fe
ranjanistic <priyanshuranjan88@gmail.com>
2022-04-05 19:49:07 +0530

    Update static libraries

--- static.py
+++ static.py
@@ -6,12 +6,14 @@ import requests
 with open('static.json', 'r') as static:
     statics = json.loads(static.read())
     for key in statics.keys():
+        print(key)
         for path in statics[key]:
             filename = path.split("/")[-1]
             finalpath = Path.joinpath(Path(os.getcwd()), "static", key, filename)
+            print(finalpath)
             response = requests.get(path)
             with open(finalpath, 'w') as f:
                 f.write(response.text)
-                print(key, "DONE")
                 f.close()
                 response.close()
+                print("DONE")
