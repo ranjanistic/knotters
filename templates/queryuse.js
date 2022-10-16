b09fa58b77bb9fbe8d0a2ded7c6900cd502002e2
Priyanshu Ranjan <priyanshuranjan88@gmail.com>
2022-10-16 15:57:24 +0530

    Update queryuse.js
    Signed-off-by: Priyanshu Ranjan <priyanshuranjan88@gmail.com>

--- templates/queryuse.js
+++ templates/queryuse.js
@@ -2,6 +2,7 @@ if ("{{request.GET.e}}") error("{{request.GET.e}}");
 if ("{{request.GET.a}}") message("{{request.GET.a}}");
 if ("{{request.GET.s}}") success("{{request.GET.s}}");
 if ("{{request.GET.report}}" === "1") reportFeedbackView();
+if ("{{request.GET.contact}}" === "1") contactRequestDialog();
 if ("{{request.GET.formerr}}") error("{{request.GET.formerr}}");
 if ("{{request.GET.msg}}") message("{{request.GET.msg}}");
 if ("{{request.GET.menu}}" == "1") openMenu(); 
