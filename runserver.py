b9414a148c192194e24b29bfafbc19b24a215a77
ranjanistic <priyanshuranjan88@gmail.com>
2023-03-03 22:17:16 +0530

    update workflows

---
+++ runserver.py
@@ -0,0 +1,24 @@
+#!~/Env/knotters/bin python3
+# -*- coding: utf-8 -*-
+
+from multiprocessing import cpu_count
+import sys
+import os
+from main.__version__ import VERSION
+from os import environ
+
+print(VERSION)
+print(sys.prefix)
+environ.setdefault('ENVPATH', 'main/.env')
+
+wsgi_app = "main.wsgi:application"
+capture_output = True
+pidfile = "/var/www/gunicorn/knotters.pid"
+# daemon = True
+bind = "unix:/var/www/sockets/knotters.gunicorn.sock"
+max_requests = 1000
+accesslog = "/var/log/gunicorn/knotters.access.log"
+errorlog = "/var/log/gunicorn/knotters.error.log"
+worker_class = 'gevent'
+workers = cpu_count()
+
