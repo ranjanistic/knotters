9814360cec7ab99c76a1d34ccd5fc546219c8bef
Divy611 <divy.parikh@hotmail.com>
2023-04-04 01:14:45 +0530

    Changes to be committed: 	modified:   api/urls.py 	modified:   howto/models.py 	modified:   learn/views.py

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from main.strings import URL
-from learn.views import *
+#from learn.views import updatelesson
 from .views import *
 
 urlpatterns = [
     path(URL.Api.VERIFY_CREDENTIALS, verifyCredentials),
     path(URL.Api.REFRESH_TOKEN, refreshToken),
     path(URL.Api.USER, tokenUser),
-    path(URL.Api.LESSON_UPDATE,updatelesson),
+    #path(URL.Api.LESSON_UPDATE,updatelesson),
 
 ]
