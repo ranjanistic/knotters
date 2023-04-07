bc261f6142a6f5f27531c7975bcac6712c2f8f96
ranjanistic <priyanshuranjan88@gmail.com>
2023-04-07 22:48:54 +0530

    add custom session check

--- main/decorators.py
+++ main/decorators.py
@@ -126,6 +126,9 @@ def dev_only(function: callable) -> callable:
 
     return wrap
 
+from django.shortcuts import resolve_url
+from urllib.parse import urlparse
+from django.contrib.auth.views import redirect_to_login
 
 def verified_email_required(
     function: callable = None, login_url: str = None, redirect_field_name: str = REDIRECT_FIELD_NAME
@@ -142,8 +145,22 @@ def verified_email_required(
         callable: The decorated function.
     """
     def decorator(view_func):
-        @login_required(redirect_field_name=redirect_field_name, login_url=login_url)
+     #   @login_required(redirect_field_name=redirect_field_name, login_url=login_url)
         def _wrapped_view(request: WSGIRequest, *args, **kwargs):
+            if not request.user.is_authenticated:
+                if request.headers.get('Content-Type', None) == Code.APPLICATION_JSON:
+                    return respondJson(Code.NO, error='Session expired.', status=401)
+                path = request.build_absolute_uri()
+                resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
+                # If the login url is the same scheme and net location then just
+                # use the path as the "next" url.
+                login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
+                current_scheme, current_netloc = urlparse(path)[:2]
+                if (not login_scheme or login_scheme == current_scheme) and (
+                    not login_netloc or login_netloc == current_netloc
+                ):
+                    path = request.get_full_path()
+                return redirect_to_login(path, resolved_login_url, redirect_field_name)
             if not EmailAddress.objects.filter(
                 user=request.user, verified=True
             ).exists() and not ISTESTING:
