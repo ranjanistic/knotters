65967e88f8edd59c5ad5efb4978d79a3d9589d78
Divy611 <divy.parikh@hotmail.com>
2023-04-11 13:25:59 +0530

    Changes to be committed: 	modified:   learn/serializers.py

--- learn/serializers.py
+++ learn/serializers.py
@@ -1,28 +1,23 @@
-#from rest_framework import serializers
-#from.models import *
-#from people.models import User
-#
-#class UserSerializer(models.Model):#serializers.ModelSerializer:
-#    #date_joined = serializers.ReadOnlyField()
-#    class Meta(object):
-#        model = User
-#        fields = ('id', 'email', 'first_name', 'last_name','date_joined', 'password')
-#        extra_kwargs = {'password': {'write_only': True}}
-#
-#class CourseSerializer(models.Model):
-#    class Meta(object):
-#       model=Course
-#       fields=('courseID','title','short_desc','long_desc','lessons','raters','lessonCount','likes','creator')
-#       extra_kwargs={}
-#
-#class LessonSerializer(models.Model):
-#    class Meta(object):
-#        model=Lesson
-#        fields=('id','name','lessontype','course')
-#        extra_kwargs={}
-#
-#class RatingSerializer(models.Model):
-#    class Meta(object):
-#        model=rating
-#        fields=('rating','review','creator','courseID')
-#        extra_kwargs={}
+from rest_framework import serializers
+from.models import *
+from people.models import User
+
+class UserSerializer(models.Model):
+    class Meta(object):
+        model=User
+        fields = "__all__"
+
+class CourseSerializer(models.Model):
+    class Meta(object):
+       model=Course
+       fields="__all__"
+
+class LessonSerializer(models.Model):
+    class Meta(object):
+        model=Lesson
+        fields="__all__"
+
+class RatingSerializer(models.Model):
+    class Meta(object):
+        model=rating
+        fields="__all__"
